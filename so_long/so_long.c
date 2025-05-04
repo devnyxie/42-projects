c2e12372e66eb7877e63276eab2e6844c4c61a8c
Tim Afanasiev <soupsoupchannel@gmail.com>
2025-05-04 23:30:31 +0200

    visual improvements

--- so_long/so_long.c
+++ so_long/so_long.c
@@ -6,13 +6,14 @@
 /*   By: tafanasi <tafanasi@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/26 22:26:09 by tafanasi          #+#    #+#             */
-/*   Updated: 2025/04/27 00:54:41 by tafanasi         ###   ########.fr       */
+/*   Updated: 2025/05/04 03:10:34 by tafanasi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+#include <X11/keysym.h> // For key codes
 
-#define TILE 32 // the size of a tile in pixels
+#define TILE 64 // the size of a tile in pixels
 
 typedef struct s_game {
 	void	*mlx;
@@ -31,11 +32,11 @@ void	load_images(t_game *game)
 	int	w;
 	int	h;
 	
-	game->img_wall = mlx_xpm_file_to_image(game->mlx, "wall.xpm", &w, &h);
-	game->img_floor = mlx_xpm_file_to_image(game->mlx, "floor.xpm", &w, &h);
-	// game->img_player = mlx_xpm_file_to_image(game->mlx, "player.xpm", &w, &h);
-	// game->img_exit = mlx_xpm_file_to_image(game->mlx, "exit.xpm", &w, &h);
-	// game->img_collect = mlx_xpm_file_to_image(game->mlx, "collect.xpm", &w, &h);
+	game->img_wall = mlx_xpm_file_to_image(game->mlx, "assets/wall.xpm", &w, &h);
+	game->img_floor = mlx_xpm_file_to_image(game->mlx, "assets/floor.xpm", &w, &h);
+	game->img_player = mlx_xpm_file_to_image(game->mlx, "assets/player.xpm", &w, &h);
+	game->img_exit = mlx_xpm_file_to_image(game->mlx, "assets/exit.xpm", &w, &h);
+	game->img_collect = mlx_xpm_file_to_image(game->mlx, "assets/collect.xpm", &w, &h);
 }
 
 void	draw_map(t_game *game, int height, int width)
@@ -44,31 +45,126 @@ void	draw_map(t_game *game, int height, int width)
 	int	j;
 
 	i = 0;
+	// floor layer
+	while(i < height)
+	{
+		j = 0;
+		while (j < width)
+		{
+			mlx_put_image_to_window(game->mlx, game->win, game->img_floor, j * TILE, i * TILE);
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	// walls, exit, coins and player layer
 	while (i < height)
 	{
 		j = 0;
 		while (j < width)
 		{
-			if (game->map[i][j] == '1'){
-				printf("drawing wall at %d %d\n", j, i);
+			if (game->map[i][j] == '1')
 				mlx_put_image_to_window(game->mlx, game->win, game->img_wall, j * TILE, i * TILE);
-
-			}
-			else if (game->map[i][j] == '0'){
-				printf("drawing floor at %d %d\n", j, i);
+			else if (game->map[i][j] == '0')
 				mlx_put_image_to_window(game->mlx, game->win, game->img_floor, j * TILE, i * TILE);
-			}
-			// else if (game->map[i][j] == 'C')
-			// 	mlx_put_image_to_window(game->mlx, game->win, game->img_collect, j * TILE, i * TILE);
-			// else if (game->map[i][j] == 'E')
-			// 	mlx_put_image_to_window(game->mlx, game->win, game->img_exit, j * TILE, i * TILE);
+			else if (game->map[i][j] == 'C')
+				mlx_put_image_to_window(game->mlx, game->win, game->img_collect, j * TILE, i * TILE);
+			else if (game->map[i][j] == 'E')
+				mlx_put_image_to_window(game->mlx, game->win, game->img_exit, j * TILE, i * TILE);
+			else if (game->map[i][j] == 'P')
+				mlx_put_image_to_window(game->mlx, game->win, game->img_player, j * TILE, i * TILE);
 			j++;
 		}
 		i++;
 	}
 }
 
+// Function to handle keypress events
+int handle_keypress(int keycode, t_game *game)
+{
+    int player_x = -1;
+    int player_y = -1;
 
+    // Find the player's current position
+    for (int i = 0; game->map[i]; i++)
+    {
+        for (int j = 0; game->map[i][j]; j++)
+        {
+            if (game->map[i][j] == 'P')
+            {
+                player_x = j;
+                player_y = i;
+                break;
+            }
+        }
+        if (player_x != -1)
+            break;
+    }
+
+    if (player_x == -1 || player_y == -1)
+        return (0); // Player not found
+
+    // Handle movement based on keycode
+
+
+	// any movement towards the exit
+	if(keycode == XK_Left && game->map[player_y][player_x - 1] == 'E') // Move left
+	{
+		handle_exit(NULL);
+		return (0);
+	}
+	else if (keycode == XK_Right && game->map[player_y][player_x + 1] == 'E') // Move right
+	{
+		handle_exit(NULL);
+		return (0);
+	}
+	else if (keycode == XK_Up && game->map[player_y - 1][player_x] == 'E') // Move up
+	{
+		handle_exit(NULL);
+		return (0);
+	}
+	else if (keycode == XK_Down && game->map[player_y + 1][player_x] == 'E') // Move down
+	{
+		handle_exit(NULL);
+		return (0);
+	}
+
+	// Prevent moving through the walls
+	
+
+	// walls
+    if (keycode == XK_Left && game->map[player_y][player_x - 1] != '1') // Move left
+    {
+        game->map[player_y][player_x] = '0';
+        game->map[player_y][player_x - 1] = 'P';
+    }
+    else if (keycode == XK_Right && game->map[player_y][player_x + 1] != '1') // Move right
+    {
+        game->map[player_y][player_x] = '0';
+        game->map[player_y][player_x + 1] = 'P';
+    }
+    else if (keycode == XK_Up && game->map[player_y - 1][player_x] != '1') // Move up
+    {
+        game->map[player_y][player_x] = '0';
+        game->map[player_y - 1][player_x] = 'P';
+    }
+    else if (keycode == XK_Down && game->map[player_y + 1][player_x] != '1') // Move down
+    {
+        game->map[player_y][player_x] = '0';
+        game->map[player_y + 1][player_x] = 'P';
+    }
+	else if (keycode == XK_Escape) // Exit the game
+	{
+		// todo: free everything
+		handle_exit(NULL);
+		return (0);
+	}
+
+    // Redraw the map
+    draw_map(game, map_height(game->map), ft_strlen(game->map[0]));
+
+    return (0);
+}
 
 int	main(int argc, char **argv)
 {
@@ -95,6 +191,7 @@ int	main(int argc, char **argv)
 	draw_map(&game, height, width);
 
 	mlx_hook(game.win, 17, 0, handle_exit, NULL); // Close window
+	mlx_hook(game.win, 2, 1L << 0, handle_keypress, &game); // KeyPress event
 	mlx_loop(game.mlx);
 
 	free_2d(game.map);
